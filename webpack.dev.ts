bbe5e6f75ff59dfb6dc6b9e6d8d977421b6e627c
Andrew Lahikainen <andrew@lahikainen.co.nz>
2019-01-26 18:31:26 +1300

    dev: add webpack config for development

---
+++ webpack.dev.ts
@@ -0,0 +1,67 @@
+import { Configuration } from 'webpack';
+import { resolve } from 'path';
+import autoprefixer from 'autoprefixer';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: Configuration = {
+  mode: 'development',
+
+  entry: resolve(__dirname, 'demo', 'index.ts'),
+
+  output: {
+    filename: '[name].[hash].js',
+    path: resolve(__dirname, 'dist')
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: resolve(__dirname, 'dist'),
+    port: 8080,
+    historyApiFallback: true,
+    publicPath: '/',
+    open: true
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts', '.scss', '.html'],
+    modules: [resolve(__dirname, 'node_modules'), resolve(__dirname, 'src')]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: resolve(__dirname, 'demo', 'index.html')
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: { loader: 'ts-loader' }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: { plugins: () => [autoprefixer()] }
+          },
+          {
+            loader: 'sass-loader',
+            options: { includePaths: [resolve(__dirname, 'node_modules')] }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: 'html-loader?exportAsEs6Default' }]
+      }
+    ]
+  }
+};
+
+export default config;
